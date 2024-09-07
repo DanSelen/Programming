9f8a1f4c716e8c39daa3bf723f540e415cf7e8f0
DanSelen <daniilselenchuk@gmail.com>
2024-09-07 20:22:50 +0700

    Исправил баги(выполнил 8 пунктов)

---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/Model/Item.cs
@@ -0,0 +1,70 @@
+﻿using ObjectOrientedPractics.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class Item
+    {
+        readonly int _id;
+        private string _name;
+        private string _info;
+        private double _cost;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.Length>200)
+                {
+                    throw new ArgumentException("Название товара не должно превышать 200 символов");
+                }
+                _name = value;
+            }
+        }
+        public string Info
+        {
+            get
+            {
+                return _info;
+            }
+            set
+            {
+                if (value.Length>1000)
+                {
+                    throw new ArgumentException("Описание товара не должно превышать 1000 символов");
+                }
+                _info = value;
+            }
+        }
+        public double Cost
+        {
+            get
+            {
+                return _cost;
+            }
+            set
+            {
+                if (value>1000000 || value < 0 )
+                {
+                    throw new ArgumentException("Цена должна быть в диапазоне от 0 до 1000");
+                }
+                _cost = value;
+            }
+        }
+        public Item(string name, string info, double cost)
+        {
+            Name = name;
+            Info = info;
+            Cost = cost;
+            _id = IdGenerator.GetNextId();
+        }
+    }
+}
---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/Services/IdGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Services
+{
+    internal class IdGenerator
+    {
+        private static int _counter;
+        public static int GetNextId()
+        {
+            return ++_counter;
+        }
+    }
+}
